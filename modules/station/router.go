9fde7b755822a86139b56444b320c8730c9dfe16
Dito-7 <112498814+Dito-7@users.noreply.github.com>
2025-07-19 23:53:41 +0700

    feat: add routes schedules

--- modules/station/router.go
+++ modules/station/router.go
@@ -14,6 +14,10 @@ func Initiate(router *gin.RouterGroup) {
 	station.GET("", func(ctx *gin.Context) {
 		GetAllStations(ctx, stationService)
 	})
+
+	station.GET("/:id", func(ctx *gin.Context) {
+		CheckScheduleByStation(ctx, stationService)
+	})
 }
 
 func GetAllStations(ctx *gin.Context, service Service) {
@@ -30,6 +34,27 @@ func GetAllStations(ctx *gin.Context, service Service) {
 	ctx.JSON(http.StatusOK, response.APIResponse{
 		Success: true,
 		Message: "Successfully Get All stations",
+		Data:    datas,
+	})
+}
+
+func CheckScheduleByStation(ctx *gin.Context, service Service) {
+	id := ctx.Param("id")
+
+	datas, err := service.CheckScheduleByStation(id)
+
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, response.APIResponse{
+			Success: false,
+			Message: err.Error(),
+			Data:    nil,
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, response.APIResponse{
+		Success: true,
+		Message: "Successfully Get Schedules by Station",
 		Data:    datas,
 	})
 }
