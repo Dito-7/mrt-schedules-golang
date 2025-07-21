e8eeda71e345c4d4bd81ecd49f380bc29920c391
Dito-7 <112498814+Dito-7@users.noreply.github.com>
2025-07-21 10:33:03 +0700

    feat: add shcdule response

--- modules/station/dto.go
+++ modules/station/dto.go
@@ -9,3 +9,15 @@ type StationResponse struct {
 	NID   string `json:"nid"`
 	Title string `json:"title"`
 }
+
+type Schedule struct {
+	NID                string `json:"nid"`
+	StationName        string `json:"title"`
+	ScheduleBundaranHI string `json:"jadwal_hi_biasa"`
+	ScheduleLebakBulus string `json:"jadwal_lb_biasa"`
+}
+
+type ScheduleResponse struct {
+	StationName string `json:"station"`
+	Time        string `json:"time"`
+}
